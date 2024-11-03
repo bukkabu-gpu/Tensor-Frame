9cfb47796972347cc1b3b16cb83e4c9c253ed844
Mentally Distressed Programmer <arturmisiurev@gmail.com>
2024-11-03 13:23:58 -0500

    Fix shader_cache.rs
    I used direct path for trouble shooting, fixed

--- src/gpu_acel/shader_cache.rs
+++ src/gpu_acel/shader_cache.rs
@@ -8,7 +8,7 @@ pub(crate) static SHADER_CACHE: Lazy<HashMap<Operation, String>> = Lazy::new(|| 
 
     // Preload shaders
     for op in [Operation::Add, Operation::Sub, Operation::Mul, Operation::Div] {
-        let shader_path = format!("C:\\Users\\artur\\RustroverProjects\\tensor\\src\\gpu_acel\\shader_code\\{:?}.wgsl", op);
+        let shader_path = format!("src/gpu_acel/shader_code/{:?}.wgsl", op);
         let shader_code = fs::read_to_string(&shader_path)
             .expect(&format!("Failed to read shader file: {:?}", shader_path));
         cache.insert(op, shader_code);
