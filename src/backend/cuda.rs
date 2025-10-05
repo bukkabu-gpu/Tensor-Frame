95ae4e8e1961e41387aebdf61d70888baae08114
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-10-05 20:48:21 +0900

    broadcast_toのlaunchconfigの設定を変更

--- src/backend/cuda.rs
+++ src/backend/cuda.rs
@@ -598,7 +598,17 @@ impl Backend for CudaBackend {
                     })?;
 
                     let size = cuda_storage.buffer.len();
-                    let cfg = LaunchConfig::for_num_elems(size as u32);
+                    let block_x = 16;
+                    let block_y = 16;
+
+                    let grid_x = (out_cols + block_x - 1) / block_x;
+                    let grid_y = (out_rows + block_y - 1) / block_y;
+
+                    let cfg = LaunchConfig {
+                        grid_dim: (grid_x as u32, grid_y as u32, 1),
+                        block_dim: (block_x as u32, block_y as u32, 1),
+                        shared_mem_bytes: 0,
+                    };
 
                     let mut builder = stream.launch_builder(kernel);
                     let in_rows = from_shape.dims()[0];
